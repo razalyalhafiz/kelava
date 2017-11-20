9005bd98389f638cabdb594772b949241edcaa00
unknown <razalyalhafiz@gmail.com>
2017-11-20 19:16:57 +0800

    initial commit

---
+++ custom.js
@@ -0,0 +1,29 @@
+/* Add your JavaScript code here.
+
+If you are using the jQuery library, then don't forget to wrap your code inside jQuery.ready() as follows:
+
+jQuery(document).ready(function( $ ){
+    // Your code in here
+});
+
+--
+
+If you want to link a JavaScript file that resides on another server (similar to
+<script src="https://example.com/your-js-file.js"></script>), then please use
+the "Add HTML Code" page, as this is a HTML code that links a JavaScript file.
+
+End of comment */ 
+
+jQuery(document).ready(function( $ ){
+  	// Auto-expand FAQ and remove click event handler
+    $('.close-faq').addClass('open-faq');
+    $('.hrf-content').css('display', 'block');
+
+  	// Open social links in a new tab
+	$('.ssi-facebook a').attr('target', '_blank');
+  	$('.ssi-instagram a').attr('target', '_blank');
+  
+  	// Modify href of return to shop in cart page
+  	$('.wc-backward').attr('href', 'https://kelava.my');
+});
+
